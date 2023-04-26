187ab44a5c3d11de4427f1b2f5b91f04928bba48
Brandon <91694942+CPScout22@users.noreply.github.com>
2023-04-26 14:00:10 -0600

    Update knockoff mario.py

--- knockoff mario.py
+++ knockoff mario.py
@@ -7,6 +7,50 @@ screen.fill((0,0,0))
 clock = pygame.time.Clock() #set up clock
 gameover = False #variable to run our game loop
 
+LEFT=0
+RIGHT=1
+UP = 2
+DOWN = 3
+SPACE = 4
+
+potato = True
+
+class fireball:
+    def __init__(self):
+        self.xpos = -10 #draw offscreen when not in use
+        self.ypos = -10
+        self.isAlive = False
+        self.direction = RIGHT
+    def shoot(self, x, y, dir):
+        self.xpos = x + 20
+        self.ypos = y + 20
+        self.isAlive = True
+        self.direction = dir
+    def move(self):
+        if self.direction == RIGHT:
+            self.xpos+=20
+        elif self.direction == LEFT:
+            self.xpos-=20
+        elif self.direction == UP:
+            self.ypos-=20
+        elif self.direction == DOWN:
+            self.ypos+=20
+    
+        #add other directions here
+    def draw(self):
+        pygame.draw.circle(screen, (250, 0, 0), (self.xpos, self.ypos), 10)
+        pygame.draw.circle(screen, (250, 250, 0), (self.xpos, self.ypos), 5)
+    def collide(self, x, y):
+        if math.sqrt((self.xpos - x) ** 2 + (self.ypos - y) ** 2) < 25: #25 is radius of fireball + radius of potato
+            print("collision!")
+            return True
+        else:
+            return False
+       
+
+ball = fireball()
+
+
 #MAP: 1 is grass, 2 is brick
 map = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,0],
        [2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2 ,0 ,0, 0,0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,2],
@@ -16,7 +60,7 @@ map = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0
        [2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0 ,0 ,0, 0,0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,2],
        [2, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2 ,0 ,0, 0,0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,2],
        [2, 0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 2 ,0 ,0, 0,0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,2],
-       [2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2 ,0 ,0, 0,0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,2],
+       [2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2 ,0 ,0, 0,0,  0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,2],
        [2, 0, 2, 2, 2, 0, 0, 2, 0, 0, 0, 0, 0 ,0 ,0, 0,0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,2],
        [2, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0 ,0 ,0, 0,0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,2],
        [2, 0, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0 ,0 ,0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,2],
@@ -39,11 +83,7 @@ map = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0
 
 
 #CONSTANTS
-LEFT=0
-RIGHT=1
-UP = 2
-DOWN = 3
-SPACE = 4
+
 
 #animation variables variables
 frameWidth = 32
@@ -64,10 +104,11 @@ class Goomba():
         if ticker % 100==0:
             self.xpos+= 50*self.direction
         return time
+    def draw(self):
+        screen.blit(PotatoPic, (200 + x_offset, 200 + y_offset))
         
 
 
-fireball = pygame.image.load('fireball.png') 
 metal = pygame.image.load('metal.png') #load your spritesheet
 dirt = pygame.image.load('dirt.png')
 link = pygame.image.load('link.png') #load your spritesheet
@@ -107,6 +148,7 @@ while gameover == False:
                 keys[DOWN] = True
             elif event.key == pygame.K_SPACE:
                 keys[SPACE] = True
+        
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 keys[LEFT] = False
@@ -150,7 +192,10 @@ while gameover == False:
         movingx = False
 
     #check space for shooting
-    
+    if keys[SPACE] == True:
+        ball.shoot(xpos, ypos, direction)
+       
+    ball.move()
        
     #DOWN MOVEMENT
     if keys[DOWN] == True:
@@ -217,7 +262,9 @@ while gameover == False:
     if xpos < 0:
         xpos+=3
 
-
+    if ball.collide(215, 215) == True:
+        ball.isAlive = False
+        potato = False
 
     #physics Section
     
@@ -260,11 +307,12 @@ while gameover == False:
             if map[i][j] == 2:
                 screen.blit(metal, (j * 50 + x_offset, i * 50 + y_offset), (0, 0, 50, 50))
 
+    if ball.isAlive == True:
+            ball.draw()
 
 
 
     screen.blit(link, (xpos, ypos), (frameWidth*frameNum, RowNum*frameHeight, frameWidth, frameHeight))
 
-    screen.blit(PotatoPic, (200 + x_offset, 200 + y_offset))
 
     pygame.display.flip()#this actually puts the pixel on the screen
