d9db6269852067868466c4ef87a8aaad09b5a70d
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2023-05-09 14:42:44 -0600

    Update Rope.py

--- Rope.py
+++ Rope.py
@@ -17,7 +17,7 @@ class rope:
         self.x_offset = 0
         self.y_offset = 0  
     def draw(self, screen, xoff, yoff):
-        pygame.draw.(screen, (250, 0, 250), (self.xpos+xoff, self.ypos+yoff), 20)
+        pygame.draw.circle(screen, (250, 0, 250), (self.xpos+xoff, self.ypos+yoff), 20)
 
     def move(self, map, ticker,  px, py, xoff, yoff):
         #check if player is direct line of sight
